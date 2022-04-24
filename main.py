56665e9a7f0001f4c249868a456f7f07217e0935
vinesh27 <vinitrajpurohit00@gmail.com>
2022-04-24 20:27:58 +0530

    Improved Logging

--- main.py
+++ main.py
@@ -1,50 +1,54 @@
 from instagrapi import Client
-import json, time, random
+import json, time, random, os
 
 # Reading the config file and converting it to a dictionary
-config = ""
+config = {}
 with(open("config.json", "r")) as f: config = json.load(f)
 print("Config File Loaded")
 USERNAME = config['username']
 PASSWORD = config['password']
 USERS_TO_SCRAPE = config['usernameToScrape']
 
-# Logging into the account using the config
-cl = Client()
-cl.login(USERNAME, PASSWORD)
+def runBot(username, password):
+    if os.name == 'nt': 
+        os.system('cls')
+    else: 
+        os.system('clear')
+    
+    print("Logging into: " + username + "...")
+    # Logging into the account using the config
+    cl = Client()
+    cl.login(username, password)
+    print("Logged into: " + username)
+    for user in USERS_TO_SCRAPE:
+        print("Scraping User: " + user)
+        # Getting the user's info
+        userID = cl.user_id_from_username(user)
 
-for user in USERS_TO_SCRAPE:
-    # Getting the user's info
-    userID = cl.user_id_from_username(user)
+        # Getting the user's posts
+        posts = cl.user_medias(userID, amount=5)
 
-    # Getting the user's posts
-    posts = cl.user_medias(userID, amount=5)
+        # Looping through the posts
+        for post in posts:
+            print("    Scraping post: https://www.instagram.com/p/" + post.code)
+            # Getting all the users who liked the post
+            mediaLikers = cl.media_likers(post.id)    
+            # Loop through the likers
+            for liker in mediaLikers:
+                # Getting the userID from their username
+                userID = cl.user_id_from_username(liker.username)
+                # Checking if user is a bot (or a private account)
+                userDict = cl.user_info_by_username(liker.username).dict()
+                if str(userDict['is_private']) == "False" and userDict['profile_pic_url'] != "":
+                    cl.user_follow(userID)
+                    userPosts = cl.user_medias(userID, amount=2)
+                    for userPost in userPosts:
+                        cl.media_like(userPost.id)
+                    print("        Followed & Liked: " + str(liker.username))
+                    # Generate a random number between 5 and 10
+                    time.sleep(5 + (random.random() * 5))
+                else:
+                    print("        Skipping Bot/Private Account: " + str(liker.username))
+    print("FINISHED")
 
-    # Looping through the posts
-    for post in posts:
-        # Getting all the users who liked the post
-        mediaLikers = cl.media_likers(post.id)    
-        # Loop through the likers
-        for liker in mediaLikers:
-            # Getting the userID from their username
-            userID = cl.user_id_from_username(liker.username)
-            # Checking if user is a bot (or a private account)
-            userDict = cl.user_info_by_username(liker.username).dict()
-            if str(userDict['is_private']) == "False" and userDict['profile_pic_url'] != "":
-                # Following the user
-                cl.user_follow(userID)
-                # Gets their two recent posts
-                userPosts = cl.user_medias(userID, amount=2)
-                # Looping through those posts
-                for userPost in userPosts:
-                    # Liking the post
-                    cl.media_like(userPost.id)
-                # Simply printing their username
-                print("Followed & Liked:" + str(liker.username))
-                # Generate a random number between 5 and 10
-                time.sleep(5 + (random.random() * 5))
-            else:
-                print("Skipping Bot/Private Account: " + str(liker.username))
-        break
-
-print("FINISHED")
+runBot(USERNAME, PASSWORD)
