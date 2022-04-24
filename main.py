f884fb6a6d501568e6f0d1351a3172efdc76decf
vinesh27 <vinitrajpurohit00@gmail.com>
2022-04-24 20:08:47 +0530

    Added check for bot & private accounts

--- main.py
+++ main.py
@@ -4,41 +4,47 @@ import json, time, random
 # Reading the config file and converting it to a dictionary
 config = ""
 with(open("config.json", "r")) as f: config = json.load(f)
-
+print("Config File Loaded")
 USERNAME = config['username']
 PASSWORD = config['password']
-USERNAME_TO_SCRAPE = config['usernameToScrape']
+USERS_TO_SCRAPE = config['usernameToScrape']
 
 # Logging into the account using the config
 cl = Client()
 cl.login(USERNAME, PASSWORD)
 
-# Getting the user's info
-userID = cl.user_id_from_username(USERNAME_TO_SCRAPE)
+for user in USERS_TO_SCRAPE:
+    # Getting the user's info
+    userID = cl.user_id_from_username(user)
 
-# Getting the user's posts
-posts = cl.user_medias(userID, amount=5)
+    # Getting the user's posts
+    posts = cl.user_medias(userID, amount=5)
 
-# Looping through the posts
-for post in posts:
-    # Getting all the users who liked the post
-    mediaLikers = cl.media_likers(post.id)    
-    # Loop through the likers
-    for liker in mediaLikers:
-        # Getting the userID from their username
-        userID = cl.user_id_from_username(liker.username)
-        # Following the user
-        cl.user_follow(userID)
-        # Gets their two recent posts
-        userPosts = cl.user_medias(userID, amount=2)
-        # Looping through those posts
-        for userPost in userPosts:
-            # Liking the post
-            cl.media_like(userPost.id)
-        # Simply printing their username
-        print(liker.username)
-        # Generate a random number between 5 and 10
-        time.sleep( 5 + (random.random() * 5))
-    break
+    # Looping through the posts
+    for post in posts:
+        # Getting all the users who liked the post
+        mediaLikers = cl.media_likers(post.id)    
+        # Loop through the likers
+        for liker in mediaLikers:
+            # Getting the userID from their username
+            userID = cl.user_id_from_username(liker.username)
+            # Checking if user is a bot (or a private account)
+            userDict = cl.user_info_by_username(liker.username).dict()
+            if str(userDict['is_private']) == "False" and userDict['profile_pic_url'] != "":
+                # Following the user
+                cl.user_follow(userID)
+                # Gets their two recent posts
+                userPosts = cl.user_medias(userID, amount=2)
+                # Looping through those posts
+                for userPost in userPosts:
+                    # Liking the post
+                    cl.media_like(userPost.id)
+                # Simply printing their username
+                print("Followed & Liked:" + str(liker.username))
+                # Generate a random number between 5 and 10
+                time.sleep(5 + (random.random() * 5))
+            else:
+                print("Skipping Bot/Private Account: " + str(liker.username))
+        break
 
 print("FINISHED")
