ad097ed24818497f3a572c06b15a2509100f86b4
vinesh27 <vinitrajpurohit00@gmail.com>
2022-04-29 13:48:25 +0530

    Added Delay & Fixed Bot Detection

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ USERNAME = config['username']
 PASSWORD = config['password']
 USERS_TO_SCRAPE = config['usernameToScrape']
 
-def runBot(username, password):
+
+def userScrapeFollowLike(username, password):
     if os.name == 'nt': os.system('cls')
     else: os.system('clear')
-    
+
     print("Logging into: " + username + "...")
     # Logging into the account using the config
     cl = Client()
@@ -24,33 +25,38 @@ def runBot(username, password):
         print("Scraping User: " + user)
         # Getting the user's info
         userID = cl.user_id_from_username(user)
+        time.sleep(5 + (random.random() * 5))
 
         # Getting the user's posts
-        posts = cl.user_medias(userID, amount=5)
+        posts = cl.user_medias(int(userID), amount=5)
+        time.sleep(5 + (random.random() * 5))
 
         # Looping through the posts
         for post in posts:
             print("    Scraping post: https://www.instagram.com/p/" + post.code)
             # Getting all the users who liked the post
-            mediaLikers = cl.media_likers(post.id)    
+            mediaLikers = cl.media_likers(post.id)
+            time.sleep(5 + (random.random() * 5))
+
             # Loop through the likers
             for liker in mediaLikers:
                 # Getting the userID from their username
                 userID = cl.user_id_from_username(liker.username)
                 # Checking if user is a bot (or a private account)
                 userDict = cl.user_info_by_username(liker.username).dict()
-                if str(userDict['is_private']) == "False" and userDict['profile_pic_url'] != "":
+                if str(userDict['is_private']) == "False" or userDict['profile_pic_url'] != "":
                     time.sleep(5 + (random.random() * 5))
                     cl.user_follow(userID)
-                    time.sleep(3)
-                    userPosts = cl.user_medias(userID, amount=2)
+                    time.sleep(5 + (random.random() * 5))
+                    userPosts = cl.user_medias(int(userID), amount=2)
                     for userPost in userPosts:
                         cl.media_like(userPost.id)
-                        time.sleep(5)
+                        time.sleep(5 + (random.random() * 5))
                     print("        Followed & Liked: " + str(liker.username))
-                    time.sleep(15 + (random.random() * 5)) 
+                    time.sleep(25 + (random.random() * 5))
                 else:
                     print("        Skipping Bot/Private Account: " + str(liker.username))
     print("FINISHED")
 
-runBot(USERNAME, PASSWORD)
+
+userScrapeFollowLike(USERNAME, PASSWORD)
